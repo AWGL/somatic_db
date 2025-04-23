dacc8be885669f324f6fa054c0cf083e0405622f
niamh <niamh.teague@wales.nhs.uk>
2025-04-23 08:38:46 +0100

    updating knowledge base

--- somatic_variant_db/settings.py
+++ somatic_variant_db/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'auditlog',
     'analysis',
     'classify',
-    'swgs'
+    'swgs',
+    'knowledge_base'
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,7 @@ WSGI_APPLICATION = 'somatic_variant_db.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-DB_INSTANCE = 'webserver'
+DB_INSTANCE = 'local'
 
 if DB_INSTANCE == 'webserver':
     DB_PASSWORD_FILE = 'password.txt'
